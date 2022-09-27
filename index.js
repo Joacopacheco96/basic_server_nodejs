65de195adf701b4f70cfa470999197e5fa10cec3
Joacopacheco96 <97853299+Joacopacheco96@users.noreply.github.com>
2022-09-27 14:30:24 -0300

    basic server nodejs

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-
-const figuritas = require('./controllers/figuritas.js');
-const users = require('./controllers/users.js');
-
-const {
-    TOKEN_SECRET,
-    verifyToken,
-  } = require("./middlewares/jwt-verification.js");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/user', users);
-app.use('/figuritas',verifyToken, figuritas);
+const somecontroller = require("./controllers/EXAMPLE.js");
 
+const {
+  TOKEN_SECRET,
+  verifyToken,
+} = require("./middlewares/jwt-verification.js");
+
+app.use("/RUTACUALQUIERA", verifyToken, somecontroller);
 
 app.listen(3001, function () {
-    console.log('App corriendo en el puerto 3001');
+  console.log("App corriendo en el puerto 3001");
 });
