a379eac3e9309d91c7a1f87b7222212dfba459f6
Joacopacheco96 <97853299+Joacopacheco96@users.noreply.github.com>
2022-09-28 22:13:53 -0300

    update

--- db/db.js
+++ db/db.js
@@ -4,8 +4,8 @@ const config = {
     host: '127.0.0.1',
     port:'5432',
     user:'postgres',
-    password:'4884',
-    database:'postgres'
+    password:'',
+    database:''
     };
 
 const pool = new Pool(config)
